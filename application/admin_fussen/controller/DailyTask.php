cf505427dedbf68ba1338f5edf0362a8eab05dab
zhang <wei_06@126.com>
2018-06-27 00:24:30 +0800

    Merge branch 'temp'
    # Conflicts:
    #	application/admin_fussen/behavior/Period.php
    #	application/common/behavior/Period.php
    #	application/common/behavior/TaskData.php
    #	application/tags.php

---
+++ application/admin_fussen/controller/DailyTask.php
@@ -0,0 +1,72 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018-2018 http://www.donglixia.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 十万马 <962863675@qq.com>
+// +----------------------------------------------------------------------
+// | DateTime: 2018-02-09 16:17
+// +----------------------------------------------------------------------
+
+namespace app\admin_fussen\controller;
+
+use think\Controller;
+use think\Db;
+
+class DailyTask extends Controller
+{
+    /**
+     * 执行全部
+     * @param int $time 默认每隔3600秒执行一次（一个小时）
+     * @return bool
+     */
+    public function all($time = 10)
+    {
+        $file = STATIC_PATH. '/logs/daily_task/' . date("Ymd", time()) . '.log';
+        $logs = log_read($file);
+        $time_log = end($logs)['time'];
+        if (empty($logs) || (time() - strtotime($time_log) >= $time)) {
+//            $this->test();//测试
+            $this->articleGoodsPublic();//文章、产品定时发布
+            $this->deleteTempFile();//删除三天前的临时图片
+            log_write('daily_task','执行成功！');
+        }
+        die('success');
+    }
+
+    /**
+     * 测试
+     */
+    public function test()
+    {
+        Db::name('user_account_log')->insert(['user_id'=>3, 'remark'=>'测试定时任务']);
+    }
+
+    /**
+     * 文章、产品定时发布
+     */
+    public function articleGoodsPublic()
+    {
+        //文章定时发布
+        $article = Db::name('article')->where('state', 0)->where('public_time', '<=', time())->column('art_id');
+        foreach ($article as $k => $v) {
+            Db::name('article')->where('art_id', $v)->update(['state' => 1]);
+        }
+        //产品定时发布
+        $goods = Db::name('goods')->where('state', 0)->where('public_time', '<=', time())->column('goods_id');
+        foreach ($goods as $k => $v) {
+            Db::name('goods')->where('goods_id', $v)->update(['state' => 1]);
+        }
+    }
+
+    /**
+     * 删除临时文件
+     */
+    public function deleteTempFile()
+    {
+        delete_file_by_time(STATIC_PATH.'/img/temp', 72);//删除三天前的临时图片
+        delete_file_by_time(STATIC_PATH.'/img/temp', 168);//删除七天前的static/logs日志文件
+    }
+
+
+}
+
---
+++ application/common/behavior/TaskData.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018-2018 http://www.donglixia.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 十万马 <962863675@qq.com>
+// +----------------------------------------------------------------------
+// | DateTime: 2018-02-09 16:17
+// +----------------------------------------------------------------------
+
+namespace app\common\behavior;
+
+use think\Request;
+
+class TaskData
+{
+    /**
+     * 定时任务
+     */
+    public function run()
+    {
+        $request = Request::instance();
+        $url = $request->domain() . '/admin_fussen/daily_task/all.html';
+        sock_open($url);//定时任务非阻塞模式
+//        header('Location: '.$url);//测试
+    }
+
+
+
+}
+
